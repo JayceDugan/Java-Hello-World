e7181a61e75f01be35c3465c5050f82cf3abbdf7
Jayce Dugan <jgtdsoftware@gmail.com>
2023-02-15 20:42:14 +1000

    feat: Datatypes

---
+++ src/DataTypes.java
@@ -0,0 +1,19 @@
+public class DataTypes {
+    int a = 10, b = 20;
+    int c, d, e;
+    byte F = 22;
+    double pi =  3.14159;
+    char g = 'a';
+
+    public void printData() {
+        System.out.print(a);
+        System.out.print(b);
+        System.out.print(c);
+        System.out.print(d);
+        System.out.print(e);
+        System.out.print(F);
+        System.out.print(pi);
+        System.out.println(g);
+    }
+    public static void main(String args[]) {}
+}
